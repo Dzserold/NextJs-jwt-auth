32d732807e07a5171c465e104a3b8bd8d8b551c6
Dzserold <f.dzserold@gmail.com>
2024-03-19 23:15:37 +0100

    validate user email and password on server

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,3 +1,52 @@
+import validateEmail from "@/helpers/validateEmail";
+import validatePassword from "@/helpers/validatePassword";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
 export async function POST(req: Request) {
+  // Extract data sent in
+  const body = await req.json();
+  const { email, password } = body;
+
+  // Validate data
+  if (!validateEmail(email) || !validatePassword(password))
+    return Response.json(
+      {
+        message: "Invalid email or password",
+      },
+      { status: 400 }
+    );
+
+  // Lookup the user
+  const user = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  if (!user)
+    return Response.json(
+      {
+        message: "Invalid email or password",
+      },
+      { status: 400 }
+    );
+
+  // Compate password
+  const isCorrectPassword = bcrypt.compareSync(
+    password,
+    user.password
+  );
+
+  if (!isCorrectPassword)
+    return Response.json(
+      {
+        message: "Invalid email or password",
+      },
+      { status: 400 }
+    );
+
+  // Create jwt token
+
+  // Respond jwt token
+
   return Response.json({});
 }
